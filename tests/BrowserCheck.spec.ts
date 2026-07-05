d66ec97d55882719e8d447c7ba5fb387fa988420
Shivakrishna <shivakrishna.kontham@gmail.com>
2026-07-05 13:31:58 +0530

    QAOps Commit

--- tests/BrowserCheck.spec.ts
+++ tests/BrowserCheck.spec.ts
@@ -2,4 +2,5 @@ import{test, expect} from '@playwright/test';
 test('Git Code Check', async({page})=>{
 
     page.goto('https://www.google.com/');
+    expect(await page.title()).toContain('Google');
 })
